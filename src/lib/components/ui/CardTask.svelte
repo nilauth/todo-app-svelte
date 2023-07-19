05fc602ead3bd6b41e4269043c1d876dc06e6588
0nizzr <nizzrdev@gmail.com>
2023-07-19 22:53:42 +0100

    add unplugin-icons and further layout

--- src/lib/components/ui/CardTask.svelte
+++ src/lib/components/ui/CardTask.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
 	import Badge from './Badge.svelte';
 	import { todos } from '../../../store/stores';
+	import IconClose from '~icons/ic/baseline-close';
+	import IconDelete from '~icons/ic/baseline-delete';
+
 	export let task: string;
 	export let tags: string[] = [];
 </script>
 
-<div class="w-full rounded-md border-gray-300 bg-gray-100 pt-4 px-4 mb-2">
-	<p class="pb-4">{task}</p>
+<div class="w-full rounded-md border-gray-300 bg-gray-100 pt-4 pl-4 mb-2">
+	<div class="grid grid-cols-12">
+		<p class="pb-4 col-span-10 break-words">{task}</p>
+		<IconClose class="cursor-pointer" />
+		<IconDelete class="cursor-pointer" />
+	</div>
+
 	{#if tags.length > 0}
 		<hr />
 		<div class="w-full flex py-2 gap-2 flex-wrap">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,4 +17,5 @@
 		</div>
 	</section>
 </main>
+
 <!-- <SidePanel /> -->
